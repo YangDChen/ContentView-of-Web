f9088f460aa66562520d14cf49dd9e97d516a047
Yang Chen <tybea56@gmail.com>
2022-11-07 14:53:46 +0800

    Initial Commit

---
+++ ContentView of Web/ContentView_of_WebApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView_of_WebApp.swift
+//  ContentView of Web
+//
+//  Created by Yang Chen on 2022/11/7.
+//
+
+import SwiftUI
+
+@main
+struct ContentView_of_WebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
